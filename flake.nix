1d6b09d158f116abc5c589fc46f17c8417bb264c
Marc Brinkmann <git@marcbrinkmann.de>
2026-05-25 18:16:58 +0200

    Add postgres to build dependencies

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
             pname = "pm";
             version = cargoToml.workspace.package.version;
             description = "Postgres-backed job queue with transaction-based locking";
-            nativeBuildInputs = with pkgs; [ llvmPackages.bintools ];
+            nativeBuildInputs = with pkgs; [ llvmPackages.bintools postgresql ];
 
             src = pkgs.lib.cleanSource ./.;
 
